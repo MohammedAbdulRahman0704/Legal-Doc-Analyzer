e08cb57b297c5eaadfb2a9d9d294b703096b65bc
Mohammed Abdul Rahman <abdul.rahman.190704@gmail.com>
2025-05-17 17:44:50 +0530

    Implemented few CSS for frontend interface

--- frontend/app.py
+++ frontend/app.py
@@ -8,3 +8,32 @@ st.set_page_config(
     initial_slidebar_state="expanded"
 )
 
+# Custom styles fo enterprise look
+st.markdown("""
+            <style>
+            body{
+                background-color: #f3f6fb;
+            }
+            .stTextArea textarea {
+                background-color: black;
+                border-radius: 10px;
+                font-size: 15px;
+                border: 1px solid #d1d5db;
+            }
+            .stButton > button {
+                background-color: #205081;
+                color: #ffffff;
+                border-radius: 6px;
+                padding: 8px 16px;
+                font-weight: bold;
+            }
+            .stButton > button:hover {
+                background-color: #153a5b;
+            }
+            .stFileUploader {
+                margin-top: 10px;
+            }
+            .reportview-container .main footer {visibility: hidden;}
+            </style>
+            """, unsafe_allow_html=True)
+
